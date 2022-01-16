3525c75bde4af2f42d57dbff868eb92fe976dbde
Karol Pastewski <karolpastewski29@gmail.com>
2022-01-16 20:15:09 +0100

    Zmiana nazwy okna

--- Klient/KlientForm.Designer.cs
+++ Klient/KlientForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // KlientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "KlientForm";
+            this.Text = "Klient";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
