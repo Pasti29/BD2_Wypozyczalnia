f85d69cb11aad46ed58b7afae742ec78a2640ce9
Karol Pastewski <karolpastewski29@gmail.com>
2022-01-18 13:55:32 +0100

    Zmiana koloru przycisków po kliknięciu

--- Klient/KlientForm.Designer.cs
+++ Klient/KlientForm.Designer.cs
@@ -30,8 +30,10 @@
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnKatalog = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,13 +47,63 @@
             // panelMenu
             // 
             this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(52)))), ((int)(((byte)(77)))));
-            this.panelMenu.Controls.Add(this.button1);
+            this.panelMenu.Controls.Add(this.btnLogout);
+            this.panelMenu.Controls.Add(this.btnKatalog);
+            this.panelMenu.Controls.Add(this.btnLogin);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
             this.panelMenu.Name = "panelMenu";
             this.panelMenu.Size = new System.Drawing.Size(220, 681);
             this.panelMenu.TabIndex = 1;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(0, 240);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(220, 80);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Wyloguj się";
+            this.btnLogout.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnKatalog
+            // 
+            this.btnKatalog.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnKatalog.FlatAppearance.BorderSize = 0;
+            this.btnKatalog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKatalog.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnKatalog.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnKatalog.Location = new System.Drawing.Point(0, 160);
+            this.btnKatalog.Name = "btnKatalog";
+            this.btnKatalog.Size = new System.Drawing.Size(220, 80);
+            this.btnKatalog.TabIndex = 2;
+            this.btnKatalog.Text = "Wyświetl katalog";
+            this.btnKatalog.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnKatalog.UseVisualStyleBackColor = true;
+            this.btnKatalog.Click += new System.EventHandler(this.btnKatalog_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogin.Location = new System.Drawing.Point(0, 80);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(220, 80);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Zaloguj się";
+            this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // panelLogo
             // 
@@ -61,21 +113,6 @@
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(220, 80);
             this.panelLogo.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(0, 80);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(220, 80);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // KlientForm
             // 
@@ -95,7 +132,9 @@
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel panelMenu;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnKatalog;
     }
 }
--- Klient/KlientForm.cs
+++ Klient/KlientForm.cs
@@ -12,10 +12,54 @@ namespace Klient
 {
     public partial class KlientForm : Form
     {
+        private Button currentButton;
+
         public KlientForm()
         {
             InitializeComponent();
         }
 
+        private void ActivateButton(object btnSender)
+        {
+            if(btnSender != null)
+            {
+                if (currentButton != (Button) btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button) btnSender;
+                    currentButton.BackColor = Color.FromArgb(170, 164, 255);
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Segoe UI", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if(previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(50, 52, 77);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                }
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnKatalog_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
     }
 }
