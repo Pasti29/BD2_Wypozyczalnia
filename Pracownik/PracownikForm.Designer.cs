9a688ea3c4f139584c2e91452031aa79001438a6
wiktord2000 <wiktord2000@wp.pl>
2022-01-18 13:50:42 +0100

    Implementacja zachwania zakładek

--- Pracownik/PracownikForm.Designer.cs
+++ Pracownik/PracownikForm.Designer.cs
@@ -34,9 +34,13 @@
             this.btnDodajUzytkownika = new System.Windows.Forms.Button();
             this.btnModyfikowanieKat = new System.Windows.Forms.Button();
             this.btnKatalogSamochodow = new System.Windows.Forms.Button();
-            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnLogowanie = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelContent = new System.Windows.Forms.Panel();
             this.panelMenu.SuspendLayout();
+            this.panelTitle.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -47,7 +51,7 @@
             this.panelMenu.Controls.Add(this.btnDodajUzytkownika);
             this.panelMenu.Controls.Add(this.btnModyfikowanieKat);
             this.panelMenu.Controls.Add(this.btnKatalogSamochodow);
-            this.panelMenu.Controls.Add(this.btnLogin);
+            this.panelMenu.Controls.Add(this.btnLogowanie);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
@@ -70,7 +74,7 @@
             this.btnWyloguj.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnWyloguj.FlatAppearance.BorderSize = 0;
             this.btnWyloguj.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnWyloguj.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnWyloguj.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnWyloguj.ForeColor = System.Drawing.Color.White;
             this.btnWyloguj.Location = new System.Drawing.Point(0, 400);
             this.btnWyloguj.Name = "btnWyloguj";
@@ -85,13 +89,13 @@
             this.btnDodajUzytkownika.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnDodajUzytkownika.FlatAppearance.BorderSize = 0;
             this.btnDodajUzytkownika.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDodajUzytkownika.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDodajUzytkownika.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnDodajUzytkownika.ForeColor = System.Drawing.Color.White;
             this.btnDodajUzytkownika.Location = new System.Drawing.Point(0, 320);
             this.btnDodajUzytkownika.Name = "btnDodajUzytkownika";
             this.btnDodajUzytkownika.Size = new System.Drawing.Size(220, 80);
             this.btnDodajUzytkownika.TabIndex = 4;
-            this.btnDodajUzytkownika.Text = "    DODAJ    UŻYTKOWNIKA";
+            this.btnDodajUzytkownika.Text = "       DODAJ       UŻYTKOWNIKA";
             this.btnDodajUzytkownika.UseVisualStyleBackColor = true;
             this.btnDodajUzytkownika.Click += new System.EventHandler(this.btnDodajUzytkownika_Click);
             // 
@@ -100,13 +104,13 @@
             this.btnModyfikowanieKat.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnModyfikowanieKat.FlatAppearance.BorderSize = 0;
             this.btnModyfikowanieKat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnModyfikowanieKat.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnModyfikowanieKat.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnModyfikowanieKat.ForeColor = System.Drawing.Color.White;
             this.btnModyfikowanieKat.Location = new System.Drawing.Point(0, 240);
             this.btnModyfikowanieKat.Name = "btnModyfikowanieKat";
             this.btnModyfikowanieKat.Size = new System.Drawing.Size(220, 80);
             this.btnModyfikowanieKat.TabIndex = 3;
-            this.btnModyfikowanieKat.Text = "MODYFIKOWANIE KATALOGU";
+            this.btnModyfikowanieKat.Text = " MODYFIKOWANIE   KATALOGU";
             this.btnModyfikowanieKat.UseVisualStyleBackColor = true;
             this.btnModyfikowanieKat.Click += new System.EventHandler(this.btnModyfikowanieKat_Click);
             // 
@@ -115,31 +119,31 @@
             this.btnKatalogSamochodow.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnKatalogSamochodow.FlatAppearance.BorderSize = 0;
             this.btnKatalogSamochodow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnKatalogSamochodow.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnKatalogSamochodow.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnKatalogSamochodow.ForeColor = System.Drawing.Color.White;
             this.btnKatalogSamochodow.Location = new System.Drawing.Point(0, 160);
             this.btnKatalogSamochodow.Name = "btnKatalogSamochodow";
             this.btnKatalogSamochodow.Size = new System.Drawing.Size(220, 80);
             this.btnKatalogSamochodow.TabIndex = 2;
-            this.btnKatalogSamochodow.Text = " KATALOG SAMOCHODÓW";
+            this.btnKatalogSamochodow.Text = "     KATALOG     SAMOCHODÓW";
             this.btnKatalogSamochodow.UseVisualStyleBackColor = true;
             this.btnKatalogSamochodow.Click += new System.EventHandler(this.btnKatalogSamochodow_Click);
             // 
-            // btnLogin
+            // btnLogowanie
             // 
-            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(166)))));
-            this.btnLogin.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnLogin.FlatAppearance.BorderSize = 0;
-            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(0, 80);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(220, 80);
-            this.btnLogin.TabIndex = 1;
-            this.btnLogin.Text = "LOGOWANIE";
-            this.btnLogin.UseVisualStyleBackColor = false;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.btnLogowanie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnLogowanie.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogowanie.FlatAppearance.BorderSize = 0;
+            this.btnLogowanie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogowanie.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLogowanie.ForeColor = System.Drawing.Color.White;
+            this.btnLogowanie.Location = new System.Drawing.Point(0, 80);
+            this.btnLogowanie.Name = "btnLogowanie";
+            this.btnLogowanie.Size = new System.Drawing.Size(220, 80);
+            this.btnLogowanie.TabIndex = 1;
+            this.btnLogowanie.Text = "LOGOWANIE";
+            this.btnLogowanie.UseVisualStyleBackColor = false;
+            this.btnLogowanie.Click += new System.EventHandler(this.btnLogowanie_Click);
             // 
             // panelLogo
             // 
@@ -150,15 +154,49 @@
             this.panelLogo.Size = new System.Drawing.Size(220, 80);
             this.panelLogo.TabIndex = 0;
             // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.panelTitle.Controls.Add(this.lblTitle);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(220, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(842, 80);
+            this.panelTitle.TabIndex = 2;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblTitle.Size = new System.Drawing.Size(842, 80);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "LOGOWANIE";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelContent
+            // 
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(220, 80);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(842, 480);
+            this.panelContent.TabIndex = 3;
+            // 
             // PracownikForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1062, 560);
+            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.panelTitle);
             this.Controls.Add(this.panelMenu);
             this.Name = "PracownikForm";
             this.Text = "Pracownik";
             this.panelMenu.ResumeLayout(false);
+            this.panelTitle.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -170,8 +208,11 @@
         private System.Windows.Forms.Button btnDodajUzytkownika;
         private System.Windows.Forms.Button btnModyfikowanieKat;
         private System.Windows.Forms.Button btnKatalogSamochodow;
-        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnLogowanie;
         private System.Windows.Forms.Button btnWyloguj;
         private System.Windows.Forms.Panel panelEnd;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panelContent;
     }
 }
--- Pracownik/PracownikForm.cs
+++ Pracownik/PracownikForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pracownik.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,8 +16,15 @@ namespace Pracownik
     public partial class PracownikForm : Form
     {
 
-        // Tutaj zmienne jakie ma zawierać okno obiekt PracownikForm
-        private Button _currentPressedButton;
+        // Tutaj zmienne jakie ma zawierać okno obiekt PracownikForm, symbol '?' oznacza, że zmienne mogą przyjmować wartość null,
+        // Symbol '_' oznacza, że zmienna jest prywatna
+
+        private Button? _currentPressedButton;
+        private Form? _displayedForm;
+        private Boolean _isLogged = true;
+
+
+
 
         /// <summary>
         /// Konstruktor PracownikForm
@@ -24,10 +32,15 @@ namespace Pracownik
         public PracownikForm()
         {
             InitializeComponent();
+
+            // Na starcie programu będzie wybrana zakładka logowania
+            OpenChildForm(new FormLogowanie(this), btnLogowanie);
         }
 
 
-        // Metody
+
+
+        // ----------------------------------------------------Metody-----------------------------------------------------
 
         /// <summary>
         /// Metoda przywracająca wszystkie przyciski do stanu pierwotnego
@@ -39,10 +52,19 @@ namespace Pracownik
                 // Dla każdego przycisku przypientego do panelu 
                 if (control.GetType() == typeof(Button)) {
 
+                    
                     // Ustawiamy jego podstawowe parametry
                     control.BackColor = Color.FromArgb(51, 51, 76);
                     control.ForeColor = Color.White;
-                    control.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                    control.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                    control.Enabled = true;
+
+                    // Jeśli użytkownik nie będzie zalogowany - wybrane przyciski będą nieaktywne
+                    if (((control == btnModyfikowanieKat) || (control == btnDodajUzytkownika) || (control == btnWyloguj)) && (!_isLogged))
+                    {
+                        control.Enabled = false;
+                    }
+                    
 
                 }
             }
@@ -51,7 +73,7 @@ namespace Pracownik
         /// <summary>
         /// Metoda wyróżniająca przycisk
         /// </summary>
-        private void ActiveButton(Object eventSender)
+        private void ActiveButton(Object? eventSender)
         {
 
             if (eventSender != null)
@@ -59,6 +81,7 @@ namespace Pracownik
                 // Sprawdzamy, czy przycisk nie jest już wyróżniony
                 if (_currentPressedButton != (Button)eventSender)
                 {
+                    // Przywrócenie przycisków do początkowego wyglądu
                     DisableButtons();
                     // Aktualizujemy _currentPressedButton
                     _currentPressedButton = (Button)eventSender;
@@ -66,35 +89,97 @@ namespace Pracownik
                     // Ustawianie jego nadzwyczajnych parametrów
                     _currentPressedButton.BackColor = Color.FromArgb(44, 44, 166);
                     _currentPressedButton.ForeColor = Color.White;
-                    _currentPressedButton.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                    _currentPressedButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+
                 }
             }
             
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Metoda zmieniająca zawartość główego panela (panelContent) na podstawie klikniętego przycisku 
+        /// </summary>
+        /// <param name="childForm"></param>
+        /// <param name="btnSender"></param>
+        private void OpenChildForm(Form childForm, object btnSender) {
+
+            // Zamykamy wcześniejszą formę, jeśli jeszcze nie zostało to zrobione
+            if (_displayedForm != null) {
+
+                _displayedForm.Close();
+            }
+            // Wyróżniamy kliknięty przycisk
+            ActiveButton(btnSender);
+
+            // Ustawienie nowego kontentu
+            _displayedForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelContent.Controls.Add(childForm);
+            childForm.BringToFront();
+            childForm.Show();
+
+            // Zmiana nagłówka w zależności od wyświetlanej zawartości 
+            lblTitle.Text = childForm.Text;
+        }
+
+        /// <summary>
+        /// Metoda logująca użytkownika
+        /// </summary>
+        public void Login() 
         {
-            ActiveButton(sender);
+            _isLogged = true;
+            // Ustawiamy na null, aby zakładki ponownie się wygenerowały, ponieważ jeśli _currentPressedButton == btnSender (sytuacja,
+            // gdy już wcześniej wybraliśmy daną zakładkę) to zakładki nie zmieniają swojego wyglądu i właściwości - my jednak chcemy wymusić
+            // ich ponowne wygenerowanie
+            _currentPressedButton = null;
+            OpenChildForm(new FormLogowanie(this), btnLogowanie);
+        }
+
+        private void btnLogowanie_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormLogowanie(this), sender);
         }
 
         private void btnKatalogSamochodow_Click(object sender, EventArgs e)
         {
-            ActiveButton(sender);
+            OpenChildForm(new FormKatalogSamochodow(), sender);
         }
 
         private void btnModyfikowanieKat_Click(object sender, EventArgs e)
         {
-            ActiveButton(sender);
+            OpenChildForm(new FormModyfikowanieKatalogu(), sender);
         }
 
         private void btnDodajUzytkownika_Click(object sender, EventArgs e)
         {
-            ActiveButton(sender);
+            OpenChildForm(new FormDodajUzytkownika(), sender);
         }
 
         private void btnWyloguj_Click(object sender, EventArgs e)
         {
-            ActiveButton(sender);
+            // Zapisanie przycisku, z którego przeszliśmy do zakładki wyloguj 
+            Button? previousButton = _currentPressedButton;
+
+            // Wyróżnienie przycisku wyloguj (spowoduje zmianę _currentPressedButton)
+            ActiveButton(btnWyloguj);
+
+            // Wyświetlenie komunikatu
+            var result = MessageBox.Show("Czy na pewno chcesz się wylogować?", "Wylogowanie", MessageBoxButtons.YesNo);
+
+            // Obsłużenie wyniku
+            if (result == DialogResult.Yes)
+            {
+                // Wylogowanie
+                _isLogged = false;
+                // Przejście do zakładki logowania
+                OpenChildForm(new FormLogowanie(this), btnLogowanie);
+            }
+            else 
+            {
+                ActiveButton(previousButton);
+            }
         }
     }
 
