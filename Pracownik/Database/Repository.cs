98c7263fc6c4cb93ddc6a185d9c25132edecd335
wiktord2000 <wiktord2000@wp.pl>
2022-01-23 14:58:24 +0100

    Dodanie Repository

---
+++ Pracownik/Database/Repository.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Pracownik.Database
+{
+    public class Repository
+    {
+        /// <summary>
+        /// Nawiązanie połączenia z naszą bazą danych zefiniowaną w ConnectionString
+        /// </summary>
+        private readonly SqlConnection _connection = new SqlConnection(Properties.Resources.ConnectionString);
+
+
+        public void AddCar(string carID, int workerID, string brand, string model, bool borrowed, int vintage, float averageConsumption, int maxSpeed, int engineCapacity, DateTime controlDate, Image image)
+        {
+
+            _connection.Open();
+            SqlCommand command = _connection.CreateCommand();
+
+            // Konwertowanie obrazu na bajty
+            var convertedImage = new ImageConverter().ConvertTo(image, typeof(Byte[]));
+            command.Parameters.AddWithValue("@image", convertedImage);
+            
+            // Wywołanie procedury dodajSamochod(...)
+            command.CommandText = $"EXEC dodajSamochod '{carID}', '{workerID}', '{brand}', '{model}', '{borrowed}', '{vintage}', '{averageConsumption}', '{maxSpeed}', '{engineCapacity}', '{controlDate}', @image ";
+            
+            // Wykonanie zapytania
+            command.ExecuteNonQuery();
+            _connection.Close();
+
+
+        }
+
+
+
+        public void UpdateCar(string carID, int workerID, string brand, string model, bool borrowed, int vintage, float averageConsumption, int maxSpeed, int engineCapacity, DateTime controlDate, Image image)
+        {
+
+            _connection.Open();
+            SqlCommand command = _connection.CreateCommand();
+
+            // Konwertowanie obrazu na bajty
+            var convertedImage = new ImageConverter().ConvertTo(image, typeof(Byte[]));
+            command.Parameters.AddWithValue("@image", convertedImage);
+
+            // Wywołanie procedury aktualizujSamochod(...)
+            command.CommandText = $"EXEC aktualizujSamochod '{carID}', '{workerID}', '{brand}', '{model}', '{borrowed}', '{vintage}', '{averageConsumption}', '{maxSpeed}', '{engineCapacity}', '{controlDate}', @image ";
+
+            // Wykonanie zapytania
+            command.ExecuteNonQuery();
+            _connection.Close();
+
+
+        }
+
+
+
+        public void DeleteCar(string carID) 
+        {
+            // Wywołanie procedury usunSamochod
+            _connection.Open();
+
+            string query = $"EXEC usunSamochod '{carID}'";
+            SqlCommand command = new SqlCommand(query, _connection);
+
+            // Wywołanie zapytania
+            command.ExecuteNonQuery();
+            _connection.Close();
+        }
+
+
+
+        public DataRow? GetCar(string carID) 
+        {
+            //utworzenie zapytania zwracającego informację o samochodzie o danym id
+            string query = "SELECT * " +
+                           $"FROM Samochody WHERE  IDsamochodu = '{carID}';";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, _connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            if (table.Rows.Count != 0)
+            {
+                return table.Rows[0];
+            }
+            else 
+            {
+                return null;
+            }
+        }
+
+
+
+        public DataRow? GetWorker(int IDpracownika) 
+        {
+            // Wywołani funkcji tabelarycznej wyszukajPracownika(IDpracownika)
+            string query = "SELECT * " +
+                           $"FROM dbo.wyszukajPracownika('{IDpracownika}');";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, _connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            if (table.Rows.Count != 0)
+            {
+                return table.Rows[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public DataTable GetCarsTable() {
+
+            //utworzenie zapytania
+            string query = "SELECT * " +
+                           "FROM Samochody ;";
+
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, _connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            return table;
+        }
+    }
+}
